7727ba01573235306f5205e93386fc2a938fdea5
matsu <50922910+mtsml@users.noreply.github.com>
2021-12-29 12:20:32 +0900

    socket追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div>
-      yubi
-    </div>
-  );
+import { Component } from "react";
+import io from "socket.io-client";
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || '8090';
+
+
+const createSocket = () => {
+  console.log('create connection');
+  return io(`${host}:${port}`, {
+    transports: ['websocket']
+  })
+};
+
+const socket = createSocket();
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        yubi
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const app = express();
+const socket = require('socket.io');
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 8090
-app.listen(port, () => {
+const port = process.env.PORT || 8090;
+const server = app.listen(port, () => {
     console.log(`server is running on port ${port}`)
+});
+
+const io = socket(server, {
+  transports: ['websocket']
+});
+
+io.on('connection', (socket) => {
+  console.log('connection');
 });
