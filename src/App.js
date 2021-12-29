d79b23eee53ed969743d6ee4b6b3227f4c3bc862
matsu <50922910+mtsml@users.noreply.github.com>
2021-12-29 21:15:52 +0900

    相手にのみ攻撃可能な実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
+
+const LOGIN = 'LOGIN';
+const UPDATE = 'UPDATE';
+const ACTION = 'ACTION';
+const END = 'END';
+
+
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || '8090';
-
-
 const createSocket = () => {
   console.log('create connection');
   return io(`${host}:${port}`, {
@@ -13,35 +18,168 @@ const createSocket = () => {
 };
 const socket = createSocket();
 
-const LOGIN = 'LOGIN';
-const UPDATE = 'UPDATE';
 
 const App = () => {
-  const [login, setLogin] = useState(false);
-  const [myTurn, setMyTurn] = useState(false);
-  const loginEmit = () => {
-    socket.emit(LOGIN, {data: 'data'});
-  }
+  const [ login, setLogin ] = useState(false);
+  const [ room, setRoom ] =useState(null);
+  const [ onGame, setOnGame ] = useState(false);
+  const [ myTurn, setMyTurn ] = useState(false);
+  const [ player, setPlayer ] = useState({});
   
   useEffect(() => {
     socket.on(LOGIN, data => {
-      const { message } = data;
-      console.log(message);
+      console.log('login')
+      const { room } = data;
+      setRoom(room);
       setLogin(true);
+    });
+
+    socket.on(UPDATE, data => {
+      console.log('update')
+      const { nextPlayer, player } = data;
+      setPlayer(player);
+      setMyTurn(nextPlayer === socket.id);
+      onGame || setOnGame(true);
     })
-  })
+
+    socket.on(END, data => {
+      console.log('end')
+      const { winner, player } = data;
+      setPlayer(player);
+      alert(winner === socket.id ? '勝ちました' : '負けました');
+      setLogin(false);
+    })
+  }, [])
 
   return (
     <div>
-      <h1>Yubi</h1>
+      <h1>Yubi Game</h1>
       {
         login
-          ? <p>login now</p>
-          : <button onClick={loginEmit}>Login</button>
+          ? <>
+              <h2>Room: {room}</h2>
+              {
+                onGame
+                  ? <GameBorad room={room} player={player} myTurn={myTurn} />
+                  : <p>waiting...</p>
+              }
+            </>
+          : <Login />
       }
     </div>
   )
-
 }
-      
+
+
+const GameBorad = (props) => {
+  const { room, player, myTurn } = props;
+  const meId = socket.id;
+  const me = player[meId];
+  const notMeId = Object.keys(player).find(p => p !== socket.id);
+  const notMe = player[notMeId];
+
+  return (
+    <div>
+      <div>
+        <p>相手</p>
+        左手{notMe.left}
+        右手{notMe.right}
+      </div>
+      <div>
+        <p>自分</p>
+        左手{me.left}
+        右手{me.right}
+      </div>
+      {myTurn && <Action room={room} me={me} meId={meId} notMe={notMe} notMeId={notMeId} />}
+    </div>
+  )
+};
+
+
+const Action = (props) => {
+  const { room, me, meId, notMe, notMeId } = props;
+
+  const [ step, setStep ] = useState(1);
+  const [ fromHand, setFromHand ] = useState(null);
+  const [ target, setTarget ] = useState(null);
+  const [ toHand, setToHand ] = useState(null);
+
+  const emitAction = () => {
+    socket.emit(ACTION, { room, notMeId, fromHand, target, toHand });
+  }
+
+  return (
+    <>
+      {step === 1 && 
+        <>
+          <p>使う手</p>
+          <button
+            disabled={me.left === 0}
+            onClick={() => { setFromHand('left'); setStep(2); }}
+          >
+            左手
+          </button>
+          <button
+            disabled={me.right === 0}
+            onClick={() => { setFromHand('right'); setStep(2); }}
+          >
+            右手
+          </button>
+        </>
+      }
+      {step === 2 && 
+        <>
+          <p>攻撃する対象</p>
+          <button onClick={() => { setTarget(notMeId); setStep(3); }}>相手</button>
+          <button onClick={() => { setTarget(meId); setStep(3); }}>自分</button>
+          <button onClick={() => { setStep(1); }}>戻る</button>
+        </>
+      }
+      {step === 3 &&
+        <>
+          <p>狙う手</p>
+          <button 
+            disabled={notMe.left === 0}
+            onClick={() => { setToHand('left'); setStep(4); }}
+          >
+            左手
+          </button>
+          <button 
+            disabled={notMe.right === 0}
+            onClick={() => { setToHand('right'); setStep(4); }}
+          >
+            右手
+          </button>
+          <button onClick={() => { setStep(2); }}>戻る</button>
+        </>
+      }
+      {step === 4 && 
+        <>
+          <p>確認</p>
+          <button onClick={emitAction}>OK</button>
+          <button onClick={() => { setStep(3); }}>戻る</button>
+        </>
+      }
+    </>
+  )    
+}
+
+const Login = () => {
+  const [ room, setRoom ] = useState(null); 
+
+  const emitLogin = (room) => {
+    socket.emit(LOGIN, { room });
+  }
+  const handleChange = (e) => {
+    setRoom(e.target.value)
+  }
+
+  return (
+    <>
+      <input label='room' onChange={(e) => handleChange(e)} />
+      <button onClick={() => emitLogin(room)}>Login</button>
+    </>
+  )
+};
+
 export default App;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const socket = require('socket.io');
 
+
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -13,8 +14,14 @@ const server = app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 });
 
+
 const LOGIN = 'LOGIN';
 const UPDATE = 'UPDATE';
+const ACTION = 'ACTION';
+const END = 'END';
+
+const rooms = {};
+
 
 const io = socket(server, {
   transports: ['websocket']
@@ -25,9 +32,69 @@ io.on('connection', (socket) => {
 
   socket.on(LOGIN, (data) => {
     console.log(LOGIN, data)
-    const { name, room } = data;
-    socket.join(room);
-    io.to(socket.id).emit(LOGIN, { message: "LOGIN"});
-    socket.broadcast.to(room).emit(UPDATE, { message: `${name} joined room` });
+    const { room } = data;
+
+    joinRoom(socket, room);
+    io.to(socket.id).emit(LOGIN, { room });
+    if (Object.keys(rooms[room].player).length === 2) {
+      io.to(room).emit(UPDATE, rooms[room]);
+    }
+  });
+
+  socket.on(ACTION, (data) => {
+    console.log(ACTION, data)
+    const { room, notMeId, fromHand, target, toHand } = data;
+    const player = rooms[room].player;
+    const meId = socket.id;
+    const me = player[meId]; 
+    const notMe = player[notMeId];
+
+    // 自分への攻撃
+    if (meId === target) {
+        
+    }
+    // 相手への攻撃
+    else {
+      const fromHandNumber = me[fromHand];
+      const toHandNumber = notMe[toHand];
+      const newHandNumber = (fromHandNumber + toHandNumber) % 5;
+      rooms[room].player[notMeId][toHand] = newHandNumber;
+      rooms[room].nextPlayer = notMeId;
+    }
+
+    const winner = judge(me, meId, notMe, notMeId);
+    if (winner) {
+      io.to(room).emit(END, { winner, player: rooms[room].player });
+      delete rooms[room];
+      socket.leave(room);
+    } else {
+      io.to(room).emit(UPDATE, rooms[room]);    
+    }
   });
 });
+
+
+const joinRoom = (socket, room) => {
+  if (!rooms[room]) {
+    rooms[room] = {
+      nextPlayer: socket.id,
+      player: {}
+    }
+  }
+  rooms[room].player[socket.id] = {
+    left: 1,
+    right: 1
+  };
+  socket.join(room);
+};
+
+
+const judge = (me, meId, notMe, notMeId) => {
+  if (me.left === 0 && me.right === 0) {
+    return notMeId;
+  }
+  if (notMe.left === 0 && notMe.right === 0) {
+    return meId;
+  }
+  return false;
+};
